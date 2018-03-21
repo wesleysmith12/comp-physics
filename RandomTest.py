d8d097249936c62d2d84bbe91b6fe91af7f75d44
Wesley Smith <wesleysmith1@mail.weber.edu>
2018-03-21 14:15:50 -0600

    second commit

---
+++ RandomTest.py
@@ -0,0 +1,25 @@
+from math import sqrt, exp, factorial
+from random import random
+import matplotlib.pyplot as plt
+import math
+
+for i in range(20):
+	print(random())
+	
+for i in range(50):
+	print(random()*10)
+	
+for i in range(1000):
+	plt.plot(random()*11, random()*11, marker="X", markersize=2, color="blue")#, linestyle="None"
+
+plt.axis("scaled")
+plt.xlim(0,10)
+plt.ylim(0,10)
+plt.xticks(range(11))
+plt.yticks(range(11))
+plt.grid()
+plt.title("1000 random points")
+plt.xlabel("x")
+plt.ylabel("y")
+	
+plt.show()	
