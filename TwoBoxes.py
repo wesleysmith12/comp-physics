7cbb913e2ebb8f495fb1fe8f1a579727a55dd900
Wesley Smith <wesleysmith1@mail.weber.edu>
2018-03-23 16:06:56 -0600

    sadghdagh

--- TwoBoxes.py
+++ TwoBoxes.py
@@ -1,18 +1,23 @@
 from math import sqrt, exp, factorial
 from random import random
 import matplotlib.pyplot as plt
+import math
 
-n = 1000 #total number of molecules
+n = 100 #total number of molecules
 nLeft = int(n/2) #number of molecules that are on the left side of the box
 time = 0
 tList = []
 nLeftList = []
 hist = []
+probabilities = []
+
+def getProbability(nn, nnLeft):
+	return (1/(2**nn))*(factorial(nn)/(factorial(nnLeft)*factorial(nn-nnLeft)))
 
 for i in range(n+1):
 	hist.append(0)
 
-while time < n:
+while time < 5000:
 	randMolecule = random()*n # choose molecules
 	
 	#move molecule to other side of box
@@ -25,6 +30,7 @@ while time < n:
 		
 	tList.append(time)
 	nLeftList.append(nLeft)
+	#probabilities.append(getProbability()*time)
 	time += 1
 
 	
@@ -35,10 +41,21 @@ while time < n:
 #plt.xlabel("Time")
 #plt.ylabel("Remaining molecules on left")
 
-plt.bar(range(n+1), hist)
+for i in range(n+1):
+	probabilities.append(getProbability(n, i)*time)
 
+plt.plot(range(n+1), probabilities, color="blue", label = "Binomial Distribution")
 plt.title("Expansion of Gas")
-plt.xlabel("nLeft")
-plt.ylabel("nleft occurances")
+plt.xlabel("time")
+plt.ylabel("probability")
 
+#probability = (1/(2**n))*(factorial(n)/(factorial(nLeft)*factorial(n-nLeft)))
+
+plt.bar(range(n+1), hist, label = "Monte Carlo results")
+
+#plt.title("Expansion of Gas")
+#plt.xlabel("nLeft")
+#plt.ylabel("nleft occurances")
+
+plt.legend()
 plt.show()
