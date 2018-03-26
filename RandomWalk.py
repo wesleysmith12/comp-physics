2cb9797ab0b6a66bcef6b9108adae5866eb4ea2d
Juan Orozco <orozcoj007@gmail.com>
2018-03-26 17:27:08 -0600

    Added RMS

--- RandomWalk.py
+++ RandomWalk.py
@@ -11,8 +11,9 @@ x = 0 # Current x position
 xList = [] # list of the x position
 time = 0 # Current time
 rand = 0 # Random var for 50%
-iterationsSteps = 300
+iterationsSteps = 1000
 walkers = 20 # Repeat the simulation several times over
+sumOfW = 0
 
 for i in range(walkers):
     x = 0
@@ -28,7 +29,10 @@ for i in range(walkers):
 
         time += 1
         xList.append(x)
+    sumOfW += x**2
     plt.plot(range(iterationsSteps), xList)
+rms = sqrt(sumOfW/walkers)
+print("RMS: ",rms)
 plt.title("Random walks: Postion V.S Time")
 plt.xlabel("Time")
 plt.ylabel("X Postion")
