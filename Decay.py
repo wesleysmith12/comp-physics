5c5601c876bbc45571553e970ff1c90ae4d9bd52
Juan Orozco <orozcoj007@gmail.com>
2018-03-30 18:56:26 -0600

    Cleaned up & Ready to go

--- Decay.py
+++ Decay.py
@@ -1,26 +1,27 @@
+# Wesley Smith & Juan Orozco
+# Project 6: Random Processes
+# Description: Simulate nuclear decay
+
 from math import sqrt, exp, factorial
 from random import random
 import matplotlib.pyplot as plt
 import math
 
-n = 10000
-start = n
-probability = .001
-t = 0
-currentProb = 0
-count = 0
-nList = []
+n = 10000 # Starting number, this number changes over time
+t = 0 # Time step 
+nList = [] # Number list
+
+# Carry out nuclear decay simulation
 while(n >= 1000):
     for i in range(n):
-        number = int(random()*1000)
-        if(number == 1):
+        number = int(random()*1000)# choose a number from 1000
+        if(number == 1):# .001 probability
             n -= 1
     nList.append(n)
     t += 1
-print(number)
-print("n is",n)
+
+# Plot graph
 plt.plot(range(t), nList)
 plt.ylim(0,10000)
-
 plt.grid()
 plt.show()
--- DecayDist.py
+++ DecayDist.py
@@ -1,38 +1,48 @@
+# Wesley Smith & Juan Orozco
+# Project 6: Random Processes
+# Description: Simulate the nuclear decay experiment 
+# may times.
+
 from math import sqrt, exp, factorial
 from random import random
 import matplotlib.pyplot as plt
 import math
 
-n = 10000
+n = 10000 # Starting number
 count = 0 # Counter to do 100 initial simulations
+countMax = 100 # The max number of simulations
 number = 0 # Random number
-occurances = []# Occurances list
+occurrences = [] # Occurrences list
 xRange = 30 # X range on the graph
-pList = []
+pList = [] # The poisson list
 
+# The poisson probability distribution function
 def poisson(k,lambdaV):
     return (1/factorial(k))*(lambdaV**k)*(math.e**(-lambdaV))
 
+# Calculate the poisson probability distribution
 for i in range(xRange):
-    pList.append(poisson(i,10)*100)
+    pList.append(poisson(i,10)*countMax)
 
+# Setup occurrences list
 for i in range(xRange):
-    occurances.append(0)
+    occurrences.append(0)
 
-while(count < 100):
+# Carry out nuclear decay simulation many times
+while(count < countMax):
     n = 10000
     for i in range(n):
         number = int(random()*1000) # .001 probability
         if(number == 1):
             n -= 1
-    occurances[10000-n] += 1
+    occurrences[10000-n] += 1
     count += 1
-print(number)
-print("n is",n)
-plt.plot(range(xRange), pList, color="red", label="Poisson dist")
-plt.title("Expansion of Gas")
-plt.xlabel("time")
-plt.ylabel("probability")
-plt.bar(range(xRange), occurances, color="blue", label="Simulation")
+
+# Plot the graphs
+plt.plot(range(xRange), pList, color="red", label="Poisson distribution")
+plt.title("Nuclear decay")
+plt.xlabel("Number of decays")
+plt.ylabel("Number of occurrences")
+plt.bar(range(xRange), occurrences, color="blue", label="Simulation")
 plt.legend()
 plt.show()
